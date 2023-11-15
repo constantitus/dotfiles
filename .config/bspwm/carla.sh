ae617507de9b6e404b1cccfbc70ed4c6f1bc2158
constantitus <constantitus@gmail.com>
2023-11-15 19:26:37 +0200

    rofi themes submodule

--- .config/bspwm/carla.sh
+++
@@ -1,3 +0,0 @@
-pactl load-module module-null-sink sink_name="pw-virtual-effects" object.linger=1 channels=2 media.class=Audio/Duplex device.description="pw-virtual-effects" && \
-pw-jack carla -n ~/.config/bspwm/config.carxp
-
